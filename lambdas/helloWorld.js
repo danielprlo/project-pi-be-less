5a837dcb29c3c4e61cf68f403b2d6c922ba5a283
Dani <daniel.perez@coolblue.nl>
2022-04-02 12:15:41 +0200

    Adjust helloWorld lambda response

--- lambdas/helloWorld.js
+++ lambdas/helloWorld.js
@@ -4,7 +4,10 @@ const docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
 AWS.config.update({region: 'eu-west-1'});
 
 const handlerFunction = async (event, context) => {
-    return "Hello world";
+    return {
+        statusCode: 200,
+        body: "HelloWorld"
+    }
 }
 
 exports.handler = async (event, context) => {
