bdc42871a4a50ec1edba41edbd3c978955d70388
Dani <daniel.perez@coolblue.nl>
2022-05-03 18:30:29 +0200

    Adjust show user questionnaire lambda and cloudfomartion

---
+++ lambdas/user/showQuestionnaire.js
@@ -0,0 +1,31 @@
+const AWS = require("aws-sdk");
+const cors = require('../../middlewares/cors');
+
+const docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+AWS.config.update({region: 'eu-west-1'});
+
+const handlerFunction = async (event, context) => {
+    const questionnaireId = event.pathParameters.id;
+    const userId = '12345';
+    const params = {
+        TableName: 'StudyData',
+        Key: {
+            'pk': 'USER#'+userId,
+            'sk': 'QUESTIONNAIRE#'+questionnaireId,
+        }
+    };
+
+    try {
+        const data = await docClient.get(params).promise();
+        return { statusCode: 200, body: JSON.stringify(data) };
+    } catch (error) {
+        return {
+            statusCode: 400,
+            body: `Could not query: ${error.stack}`
+        };
+    }
+};
+
+exports.handler = async (event, context) => {
+    return cors(await handlerFunction(event, context));
+};
