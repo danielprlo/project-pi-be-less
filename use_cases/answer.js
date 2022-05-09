8ead97e65e8b41f0c68302b26a6543a5b1f6195d
Dani <daniel.perez@coolblue.nl>
2022-05-09 08:24:14 +0200

    await for the create repository

--- use_cases/answer.js
+++ use_cases/answer.js
@@ -2,8 +2,8 @@ const answerRepository = require('../repositories/answer');
 
 const saveAnswers = async (questionnaireId, answers, attemptId) => {
     try {
-        Object.keys(answers, attemptId).map(function(questionId) {
-            answerRepository.create(questionnaireId, questionId, answers[questionId], attemptId);
+        Object.keys(answers, attemptId).map(async function (questionId) {
+            await answerRepository.create(questionnaireId, questionId, answers[questionId], attemptId);
         });
 
         return true;
