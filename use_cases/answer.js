98319b6cc0e11964f6a5c52ee01de176bad81acf
Dani <daniel.perez@coolblue.nl>
2022-05-09 08:15:26 +0200

    fix typo in use case

--- use_cases/answer.js
+++ use_cases/answer.js
@@ -3,7 +3,7 @@ const answerRepository = require('../repositories/answer');
 const saveAnswers = async (questionnaireId, answers, attemptId) => {
     try {
         Object.keys(answers, attemptId).map(function(questionId) {
-            answerRepository.create(questionnaireId, questionId, answersList[questionId], attemptId);
+            answerRepository.create(questionnaireId, questionId, answers[questionId], attemptId);
         });
 
         return true;
