e28698221c0c1a3b0fa8924a6a1a425a58ea656d
Dani <daniel.perez@coolblue.nl>
2022-05-09 06:27:46 +0200

    Generate simple unique url for questionnaire

--- use_cases/link.js
+++ use_cases/link.js
@@ -1,6 +1,5 @@
-const uuid = require('uuid');
 const generateLinkFromQuestionnaireId = (questionnaireId) => {
-    return 'http://localhost:8081/fill-questionnaire/'+questionnaireId+'/'+uuid.v4();
+    return 'http://localhost:8081/fill-questionnaire/'+questionnaireId;
 }
 
 module.exports = { generateLinkFromQuestionnaireId }
