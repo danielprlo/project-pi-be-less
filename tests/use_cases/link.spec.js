3e45d7987b58fc5ee3ffca91d5d2c047ba46027a
Dani <daniel.perez@coolblue.nl>
2022-05-19 08:58:20 +0200

    Fix broken unit test

--- tests/use_cases/link.spec.js
+++ tests/use_cases/link.spec.js
@@ -1,6 +1,7 @@
 const {
   generateLinkFromQuestionnaireId
 } = require('../../use_cases/link');
+const repository = require('../../repositories/link');
 
 describe('Link use case', () => {
   beforeEach(() => {
@@ -9,8 +10,12 @@ describe('Link use case', () => {
 
   describe('generateLinkFromQuestionnaireId', () => {
     it('Should return a correct link', async () => {
+      const promise = new Promise((resolve) => resolve());
+      const saveLinkRepoSpy = jest.spyOn(repository, 'save').mockImplementation(() => promise);
 
-      const response = generateLinkFromQuestionnaireId('123');
+      const response = await generateLinkFromQuestionnaireId('123', '123');
+
+      expect(saveLinkRepoSpy).toBeCalledTimes(1);
       expect(response).toEqual('https://dnbjqdte2wiuk.cloudfront.net/fill-questionnaire/123');
     });
   });
