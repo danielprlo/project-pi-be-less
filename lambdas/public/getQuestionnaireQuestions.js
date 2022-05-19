6cf9144e536b8122ad97aa81d451de52b6774391
Dani <daniel.perez@coolblue.nl>
2022-05-19 11:16:02 +0200

    Fixing get questions for public endpoint

--- lambdas/public/getQuestionnaireQuestions.js
+++ lambdas/public/getQuestionnaireQuestions.js
@@ -1,14 +1,11 @@
-const AWS = require("aws-sdk");
 const cors = require('../../middlewares/cors');
 const { getQuestions } = require("../../use_cases/question");
-const { getQuestionnaireIdFromLink } = require('../../use_cases/link');
 
 const handlerFunction = async (event, context) => {
     const questionnaireId = event.pathParameters.questionnaireId;
 
     try {
-        const questionnaireIdFromLink = getQuestionnaireIdFromLink(questionnaireId);
-        const data = await getQuestions(questionnaireIdFromLink);
+        const data = await getQuestions(questionnaireId);
         return { statusCode: 200, body: JSON.stringify(data) };
     } catch (error) {
         return {
